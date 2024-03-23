a45c12c21da4fbe541130056632126d852c2aa06
nootanlodhi <nootancsm19028@gmail.com>
2024-03-23 19:29:29 +0530

    Create interface and Api methods

---
+++ src/Interface/Interface.ts
@@ -0,0 +1,19 @@
+export interface IAllFood {
+    idMeal: string,
+    strMeal: string,
+    strMealThumb: string
+}
+
+export type IFoodDetails = {
+    strArea: string,
+    strCategory: string,
+    strIngredient1: string,
+    strIngredient2: string,
+    strIngredient3: string,
+    strInstructions: string,
+    strMealThumb: string
+}
+
+export interface IArea{
+    strArea: string
+}
---
+++ src/services/services.ts
@@ -0,0 +1,13 @@
+import axios from "axios"
+
+export const fetchAllIndianFood = async(selectArea:string) =>{
+    return await axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?a="+selectArea);
+}
+
+export const fetchFoodDetailsData = async(id:string) =>{
+    return await axios.get("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id)
+}
+
+export const fetchAllArea = async() =>{
+    return await axios.get("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
+}
