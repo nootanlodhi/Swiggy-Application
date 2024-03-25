8dde79b048a8b44397878958ede0b379c653ac4d
nootanlodhi <nootancsm19028@gmail.com>
2024-03-26 01:02:14 +0530

    Active navlink

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,7 +4,6 @@ import { IoSearchOutline } from "react-icons/io5";
 import { IoCartOutline } from "react-icons/io5";
 import { LuUser } from "react-icons/lu";
 import { BiSolidOffer } from "react-icons/bi";
-import { PiBowlFood } from "react-icons/pi";
 import { IoIosHelpCircleOutline } from "react-icons/io";
 import { useContext } from "react";
 import { CreateContext } from "../../App";
@@ -16,22 +15,19 @@ const Navbar = () => {
     <nav className="bg-white w-full py-4 flex flex-col lg:flex-row justify-between xs:px-5 sm:px-10 md:px-10 lg:px-30 xl:px-50 xxl:px-64 px-10">
       <NavLink to="/"><img className="w-40" src={Logo}/></NavLink>
       <div className="flex gap-10">
-        <NavLink className="flex items-center gap-1 hover:text-orange" to='/search-food'>
+        <NavLink style={({ isActive }) => {return isActive ? { color: "#ff7849" } : {};}} className="flex items-center gap-1 hover:text-orange" to='/search-food'>
           <IoSearchOutline/><p>Search</p>
         </NavLink>
-        <NavLink className="flex items-center gap-1 hover:text-orange" to='/'>
-          <PiBowlFood/><p>Ingredient Food</p>
-        </NavLink>
-        <NavLink className="xs:hidden md:flex flex items-center gap-1 hover:text-orange" to='/'>
+        <NavLink style={({ isActive }) => {return isActive ? { color: "#ff7849" } : {};}} className="xs:hidden md:flex flex items-center gap-1 hover:text-orange" to='/'>
           <BiSolidOffer/><p>Offer</p>
         </NavLink>
-        <NavLink className="xs:hidden md:flex flex items-center gap-1 hover:text-orange" to='/'>
+        <NavLink style={({ isActive }) => {return isActive ? { color: "#ff7849" } : {};}} className="xs:hidden md:flex flex items-center gap-1 hover:text-orange" to='/'>
           <IoIosHelpCircleOutline/><p>Help</p>
         </NavLink>
-        <NavLink className="flex items-center gap-1 hover:text-orange" to='/add-to-cart'>
+        <NavLink style={({ isActive }) => {return isActive ? { color: "#ff7849" } : {};}} className="flex items-center gap-1 hover:text-orange" to='/add-to-cart'>
           {cartData && cartData.length > 0 ? <div className="bg-green px-1.5 rounded-full text-white">{cartData.length}</div> : <IoCartOutline/>}<p>Cart</p>
         </NavLink>
-        <NavLink className="xs:hidden md:flex flex items-center gap-1 hover:text-orange" to='/'>
+        <NavLink style={({ isActive }) => {return isActive ? { color: "#ff7849" } : {};}} className="xs:hidden md:flex flex items-center gap-1 hover:text-orange" to='/'>
           <LuUser/><p>Sign In</p>
         </NavLink>
       </div>
